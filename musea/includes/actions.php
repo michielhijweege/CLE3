8db7e7e6d78f31aafdf28068d0b3f16453ba283c
mickb <mickbreijer@outlook.com>
2023-03-27 14:21:49 +0200

    actions.php in musea aangepast

--- musea/includes/actions.php
+++ musea/includes/actions.php
@@ -28,17 +28,8 @@ function getMuseaDetails($id)
             "imgalt" => "logo kunsthal rotterdam",
             "linktext" => "kunsthal.com",
             "link" => "https://www.kunsthal.com/nl",
-            "topatracties" => [
-                "nummer1" => "1. Sprookjesbos",
-                "nummer2" => "2. Stoomtrein",
-                "nummer3" => "3. Fata Morgana",
-                "nummer4" => "4. Droomvlucht",
-                "nummer5" => "5. Aquanura",
-                "nummer6" => "6. Carnaval Festival",
-                "nummer7" => "7. Gondoletta",
-                "nummer8" => "8. Symbolica",
-                "nummer9" => "9. Efteling Museum",
-                "nummer10" => "10. Stoomtrein"
+            "beoordeling" => [
+                "2,5"
             ],
             "openingstijden" => [
                 "nummer1" => "<b>Maandag</b> Gesloten",
