7006341d48cffc5c4289bfe62b9651bdbc98e4c3
JerrelTB <balkerjerrel@gmail.com>
2023-03-29 14:25:38 +0200

    Lijst van bioscopen is toegevoegd en werkent. Detail mist maar nog mee bezig.Afbeeldingen toegvoegt.

---
+++ bioscopen/js/bioscopen.js
@@ -0,0 +1,155 @@
+window.addEventListener('load', init);
+
+//Volgende const zijn voor het ophalen van de bioscopen en de deatails ervan uit de webservice
+const apiUrl = "http://localhost/CLE3/bioscopen/webservice";
+
+let section;
+let list;
+let detailpage;
+
+function init(){
+    list = document.getElementById("content")
+    list.addEventListener('click', ItemWasClicked)
+
+    fetch(apiUrl)
+        .then(response => {
+            if(!response.ok){
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        })
+        .then(biosSuccesHandler)
+        .catch(ajaxErrorHandler)
+
+    createdetailpagina()
+}
+
+
+function createdetailpagina(){
+    const detailpageholder = document.createElement('section')
+    detailpageholder.classList.add('detailpageholder')
+    detailpage = document.createElement('section')
+    detailpage.classList.add('detailpage')
+    detailpage.id = 'hide'
+
+    let closebutton = document.createElement("button")
+    closebutton.innerHTML = 'Lees minder'
+    closebutton.classList.add("lees")
+
+    document.body.appendChild(detailpageholder)
+    detailpageholder.appendChild(detailpage)
+    detailpage.appendChild(closebutton)
+    detailpage.addEventListener('click', ItemWasClicked)
+}
+
+function biosSuccesHandler(data){
+    for (let bios of data) {
+        console.log(bios)
+        const newbiosdiv = document.createElement('section')
+        newbiosdiv.classList.add("content-box")
+        newbiosdiv.dataset.name = bios.name;
+        list.appendChild(newbiosdiv)
+
+        fetch(apiUrl + '?id=' + bios.id)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(detailSuccesHandler)
+            .catch(ajaxErrorHandler);
+    }
+}
+
+function detailSuccesHandler(bios) {
+    console.log(bios)
+    let biosCard = document.querySelector(`.content-box[data-name='${bios.name}']`);
+    console.log(biosCard)
+
+//logo inladen
+    let logoimg = document.createElement("img")
+    logoimg.src = bios.img
+    logoimg.alt = bios.imgalt
+    logoimg.classList.add("logo-bios")
+    biosCard.appendChild(logoimg)
+
+
+
+
+//create mid content
+    let midcontent = document.createElement('section')
+    midcontent.classList.add('midcontent')
+    biosCard.appendChild(midcontent)
+
+//title inladen
+    let title = document.createElement('h2');
+    title.innerHTML = bios.name;
+    midcontent.appendChild(title);
+
+//link inladen
+    let link = document.createElement("a")
+    link.innerHTML = bios.linktekst
+    link.href = bios.link
+    link.setAttribute('target', '_blank');
+    midcontent.appendChild(link);
+
+//create right content
+    let rightcontent = document.createElement('section')
+    museaCard.appendChild(rightcontent)
+
+//top openings tijden title inladen
+    let openingstijdenh3 = document.createElement("h3")
+    openingstijdenh3.innerHTML = 'Openings tijden'
+    rightcontent.appendChild(openingstijdenh3);
+
+//top openings tijden list inladen
+    let openingstijdenlijst = document.createElement("ul")
+    rightcontent.appendChild(openingstijden);
+
+//openings tijden inladen
+    for (let t = 0; t < 7; t++) {
+        let tijdenlistitem = document.createElement("li")
+        tijdenlistitem.innerHTML = musea.openingstijden[t]
+        openingstijdenlijst.appendChild(tijdenlistitem)
+    }
+
+//creat lees button
+    let leesmeerbutton = document.createElement("button")
+    leesmeerbutton.innerHTML = 'Lees meer'
+    leesmeerbutton.classList.add("lees")
+    rightcontent.appendChild(leesmeerbutton)
+
+//creat save button
+    let savenbutton = document.createElement("button")
+    savenbutton.innerHTML = 'save ' + musea.name
+    savenbutton.id = musea.name
+    savenbutton.classList.add("save")
+    savenbutton.classList.add("notsaved")
+    rightcontent.appendChild(savenbutton)
+}
+
+
+
+
+
+
+
+
+function ajaxErrorHandler(error)
+{
+    console.log(" ")
+}
+
+function ItemWasClicked(e){
+    //lees function
+    if(e.target.classList.item(0) === "lees"){
+        if(detailpage.id === "hide"){
+            detailpage.id = "show"
+            document.body.classList.add("noscroll")
+        } else if(detailpage.id === "show"){
+            detailpage.id = "hide"
+            document.body.classList.remove("noscroll")
+        }
+    }
+}
--- bioscopen/webservice/includes/actions.php
+++ bioscopen/webservice/includes/actions.php
@@ -2,38 +2,28 @@
 /**
  * @return array
  */
-function getDishes()
+function getCinema()
 {
     return [
         [
             "id" => 1,
-            "name" => "LingeFilm",
-            "img" => "/",
-            "imgalt" => "Logo LingeFilm",
+            "name" => "LingeFilm"
         ],
         [
             "id" => 2,
-            "name" => "DoK6 Cinema",
-            "img" => "/",
-            "imgalt" => "logo Dok6 cinema",
+            "name" => "DoK6 Cinema"
         ],
         [
             "id" => 3,
-            "name" => "Dedicon",
-            "img" => "/",
-            "imgalt" => "Logo Dedicon",
+            "name" => "Dedicon"
         ],
         [
             "id" => 4,
-            "name" => "Pathé",
-            "img" => "/",
-            "imgalt" => "Logo Pathé",
+            "name" => "Pathé"
         ],
         [
             "id" => 5,
-            "name" => "Kinepolis",
-            "img" => "/",
-            "imgalt" => "Logo Kinepolis",
+            "name" => "Kinepolis"
         ]
     ];
 }
@@ -42,32 +32,62 @@ function getDishes()
  * @param $id
  * @return mixed
  */
-function getDishDetails($id)
+function getCinemaDetails($id)
 {
     $tags = [
         1 => [
+            "id" => 1,
+            "name" => "LingeFilm",
+            "img" => "img/lingefilm.png",
+            "imgalt" => "Logo LingeFilm",
+            "linktekst" => "lingefilm.nl",
+            "locatie" => ['Rijksstraatweg 64-103, 4191 SG Geldermalsen, Gelderland'],
+
+            "beschrijving" => "LingeFilm is het gezelligste filmtheater van Geldermalsen en omstreken.
+             We vertonen van september tot en met juli 3 tot 6 keer per week een film in het multifunctioneel centrum de Pluk in Geldermalsen.
+             Ons programma bestaat uit actuele films van diverse genre, 
+             zoals filmhuis-, kinder-, familie en muziekfilms, drama, komedie, thrillers, Nederlandse films, films uit Engelstalige en niet-Engelstalige landen en af en toe een mooie documentaire.
+             In onze filmzaal kunnen bijna 100 bezoekers genieten van films in prachtig beeld en met voortreffelijk geluid.",
+
+            "openingstijden" => [''],
+            "meerInformatieLink" => "https://lingefilm.nl/2019/03/03/blind-of-slechtziend-ook-jij-kan-naar-de-bioscoop/",
+            "toegankelijkheid" => "",
+            "tags" => ['Audiodiscriptie', 'Earcatch', 'Film', 'Filmtheater']
+
+        ],
+        2 => [
+            "id" => 2,
+            "name" => "DoK6 Cinema",
+            "img" => "img/dok6cinema.png",
+            "imgalt" => "Logo LingeFilm",
             "locatie" => [''],
             "beschrijving" => "Lorem ipsun",
             "openingstijden" => [''],
             "meerInformatieLink" => "https://lingefilm.nl/2019/03/03/blind-of-slechtziend-ook-jij-kan-naar-de-bioscoop/",
+            "toegankelijkheid" => "",
             "tags" => ['Audiodiscriptie', 'Earcatch']
-
-
-
-        ],
-        2 => [
-            "recipe" => "You can make this delicious Dutch meal by ...",
-            "tags" => ['unox', 'healthy', 'stamppot', 'boerenkool']
         ],
         3 => [
+            "id" => 3,
+            "name" => "Dedicon",
+            "img" => "img/lingefilm.png",
+            "imgalt" => "Logo LingeFilm",
             "recipe" => "Very nice when your grandma prepares this meal",
             "tags" => ['omnomnom']
         ],
         4 => [
+            "id" => 4,
+            "name" => "Pathé",
+            "img" => "img/lingefilm.png",
+            "imgalt" => "Logo LingeFilm",
             "recipe" => "Everytime in the city after midnight",
             "tags" => ['kapsalon', 'tasty', 'meat']
         ],
         5 => [
+            "id" => 5,
+            "name" => "Kinepolis",
+            "img" => "img/lingefilm.png",
+            "imgalt" => "Logo LingeFilm",
             "recipe" => "Specialty when on holiday in Spain",
             "tags" => ['fish']
         ],
