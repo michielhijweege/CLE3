8c1c39f5135d719b2c968dc62c0f8869609b5e87
michielhijweege <michielhijweege@hotmail.nl>
2023-03-29 12:46:28 +0200

    er word met PHP data ingeladen en met Javascript op je scherm komen.

--- pretparken/includes/actions.php
+++ pretparken/includes/actions.php
@@ -12,7 +12,15 @@ function getPretpark()
         [
             "id" => 2,
             "name" => "Walibi",
-        ]
+        ],
+        [
+            "id" => 3,
+            "name" => "Toverland",
+        ],
+        [
+            "id" => 4,
+            "name" => "Attractiepark-slagharen",
+        ],
     ];
 }
 
@@ -24,38 +32,129 @@ function getPretparkDetails($id)
 {
     $tags = [
         1 => [
+            "id" => 1,
+            "name" => "Efteling",
             "img" => "img/efteling-logo.jpeg",
             "imgalt" => "logo efteling",
             "linktext" => "efteling.com",
             "link" => "https://www.efteling.com/nl",
             "topatracties" => [
-                "nummer1" => "1. Sprookjesbos",
-                "nummer2" => "2. Stoomtrein",
-                "nummer3" => "3. Fata Morgana",
-                "nummer4" => "4. Droomvlucht",
-                "nummer5" => "5. Aquanura",
-                "nummer6" => "6. Carnaval Festival",
-                "nummer7" => "7. Gondoletta",
-                "nummer8" => "8. Symbolica",
-                "nummer9" => "9. Efteling Museum",
-                "nummer10" => "10. Stoomtrein"
+                "1. Sprookjesbos",
+                "2. Stoomtrein",
+                "3. Fata Morgana",
+                "4. Droomvlucht",
+                "5. Aquanura",
+                "6. Carnaval Festival",
+                "7. Gondoletta",
+                "8. Symbolica",
+                "9. Efteling Museum",
+                "10. Stoomtrein"
             ],
             "openingstijden" => [
-                "nummer1" => "<b>Maandag</b> van 10:00 tot 18:00",
-                "nummer2" => "<b>Dinsdag</b> van 10:00 tot 18:00",
-                "nummer3" => "<b>Woensdag</b> van 10:00 tot 18:00",
-                "nummer4" => "<b>Donderdag</b> van 10:00 tot 18:00",
-                "nummer5" => "<b>Vrijdag</b> van 10:00 tot 18:00",
-                "nummer6" => "<b>Zaterdag</b> van 10:00 tot 18:00",
-                "nummer7" => "<b>Zondag</b> van 10:00 tot 18:00"
+                "<b>Maandag</b> van 10:00 tot 18:00",
+                "<b>Dinsdag</b> van 10:00 tot 18:00",
+                "<b>Woensdag</b> van 10:00 tot 18:00",
+                "<b>Donderdag</b> van 10:00 tot 18:00",
+                "<b>Vrijdag</b> van 10:00 tot 18:00",
+                "<b>Zaterdag</b> van 10:00 tot 18:00",
+                "<b>Zondag</b> van 10:00 tot 18:00"
             ],
             "honden" => "Toegankelijkheid met een hulp- of geleidehond\nHulp- en geleidehonden zijn welkom in de Efteling. Een hulp- of geleidehond moet herkenbaar zijn door een hesje en op de dag van het bezoek worden aangemeld bij de Gastenservice (links bij de hoofdentree van de Efteling). Hulp- of geleidehonden mogen mee in alle winkels en restaurants. Ook kunnen ze mee in onderstaande attracties.\nAls de hond niet meegaat in een attractie, maar erbuiten blijft wachten, moet er een begeleider bij de hond blijven.",
             "rolstoel" => "Toegankelijkheid met een rolstoel\nBijna alle attracties hebben een speciale rolstoelingang. Wanneer je de attractie via de rolstoelingang bezoekt, kom je geen fysieke obstakels (zoals trappen) tegen en kun je buiten de gewone wachtrij wachten. De rolstoelingang geeft geen voorrang of kortere wachttijden. Bij drukte kan er ook bij de rolstoelingang een wachttijd ontstaan."
         ],
         2 => [
-            "recipe" => "You can make this delicious Dutch meal by ...",
-            "tags" => ['unox', 'healthy', 'stamppot', 'boerenkool']
-        ]
+            "id" => 2,
+            "name" => "Walibi",
+            "img" => "img/walibi-logo.jpeg",
+            "imgalt" => "logo walibi",
+            "linktext" => "efteling.com",
+            "link" => "https://www.efteling.com/nl",
+            "topatracties" => [
+                "1. Sprookjesbos",
+                "2. Stoomtrein",
+                "3. Fata Morgana",
+                "4. Droomvlucht",
+                "5. Aquanura",
+                "6. Carnaval Festival",
+                "7. Gondoletta",
+                "8. Symbolica",
+                "9. Efteling Museum",
+                "10. Stoomtrein"
+            ],
+            "openingstijden" => [
+                "<b>Maandag</b> van 10:00 tot 18:00",
+                "<b>Dinsdag</b> van 10:00 tot 18:00",
+                "<b>Woensdag</b> van 10:00 tot 18:00",
+                "<b>Donderdag</b> van 10:00 tot 18:00",
+                "<b>Vrijdag</b> van 10:00 tot 18:00",
+                "<b>Zaterdag</b> van 10:00 tot 18:00",
+                "<b>Zondag</b> van 10:00 tot 18:00"
+            ],
+            "honden" => "Toegankelijkheid met een hulp- of geleidehond\nHulp- en geleidehonden zijn welkom in de Efteling. Een hulp- of geleidehond moet herkenbaar zijn door een hesje en op de dag van het bezoek worden aangemeld bij de Gastenservice (links bij de hoofdentree van de Efteling). Hulp- of geleidehonden mogen mee in alle winkels en restaurants. Ook kunnen ze mee in onderstaande attracties.\nAls de hond niet meegaat in een attractie, maar erbuiten blijft wachten, moet er een begeleider bij de hond blijven.",
+            "rolstoel" => "Toegankelijkheid met een rolstoel\nBijna alle attracties hebben een speciale rolstoelingang. Wanneer je de attractie via de rolstoelingang bezoekt, kom je geen fysieke obstakels (zoals trappen) tegen en kun je buiten de gewone wachtrij wachten. De rolstoelingang geeft geen voorrang of kortere wachttijden. Bij drukte kan er ook bij de rolstoelingang een wachttijd ontstaan."
+        ],
+        3 => [
+            "id" => 3,
+            "name" => "Toverland",
+            "img" => "img/toverland-logo.jpeg",
+            "imgalt" => "logo toverland",
+            "linktext" => "toverland.com",
+            "link" => "https://www.toverland.com/",
+            "topatracties" => [
+                "1. x",
+                "2. x",
+                "3. x",
+                "4. x",
+                "5. x",
+                "6. x",
+                "7. x",
+                "8. x",
+                "9. x",
+                "10. x"
+            ],
+            "openingstijden" => [
+                "<b>Maandag</b> van 10:00 tot 18:00",
+                "<b>Dinsdag</b> van 10:00 tot 18:00",
+                "<b>Woensdag</b> van 10:00 tot 18:00",
+                "<b>Donderdag</b> van 10:00 tot 18:00",
+                "<b>Vrijdag</b> van 10:00 tot 18:00",
+                "<b>Zaterdag</b> van 10:00 tot 19:00",
+                "<b>Zondag</b> van 10:00 tot 19:00"
+            ],
+            "honden" => "X",
+            "rolstoel" => "X"
+        ],
+        4 => [
+            "id" => 4,
+            "name" => "Attractiepark-slagharen",
+            "img" => "img/slagharen-logo.jpeg",
+            "imgalt" => "logo slagharen",
+            "linktext" => "slagharen.com",
+            "link" => "https://slagharen.com",
+            "topatracties" => [
+                "1. x",
+                "2. x",
+                "3. x",
+                "4. x",
+                "5. x",
+                "6. x",
+                "7. x",
+                "8. x",
+                "9. x",
+                "10. x"
+            ],
+            "openingstijden" => [
+                "<b>Maandag</b> van 10:00 tot 17:00",
+                "<b>Dinsdag</b> van 10:00 tot 17:00",
+                "<b>Woensdag</b> van 10:00 tot 17:00",
+                "<b>Donderdag</b> van 10:00 tot 17:00",
+                "<b>Vrijdag</b> van 10:00 tot 17:00",
+                "<b>Zaterdag</b> van 10:00 tot 18:00",
+                "<b>Zondag</b> van 10:00 tot 18:00"
+            ],
+            "honden" => "X",
+            "rolstoel" => "X"
+        ],
     ];
 
     return $tags[$id];
