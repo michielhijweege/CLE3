af7536cdcdf6c58d038e4ce79232f85db45f5231
mickb <mickbreijer@outlook.com>
2023-04-03 16:44:31 +0200

    musea.js detailpage form aangemaakt en output getoond

--- musea/js/musea.js
+++ musea/js/musea.js
@@ -20,6 +20,7 @@ function init()
             return response.json()
         })
         .then(museaSuccesHandler)
+        .then(pageSuccesHandler)
         .catch(ajaxErrorHandler)
 
     createDetailPagina()
@@ -41,6 +42,138 @@ function createDetailPagina(){
     detailpageholder.appendChild(detailpage)
     detailpage.appendChild(closebutton)
     detailpage.addEventListener('click', ItemWasClicked)
+}
+
+//create een section
+function pageSuccesHandler(detail){
+    for (let musea of detail) {
+        console.log(detail)
+        const newdetaildiv = document.createElement('section')
+        newdetaildiv.classList.add("content-box")
+        newdetaildiv.dataset.name = musea.name;
+        list.appendChild(newdetaildiv)
+
+        fetch(apiUrl + '?id=' + musea.id)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(reviewSuccesHandler)
+            .catch(ajaxErrorHandler);
+    }
+}
+
+//laad en zet de detail data
+function reviewSuccesHandler(review) {
+    console.log(museadetails[review].name)
+    const reviewCard = document.getElementsByClassName('detailpage')[0]
+    reviewCard.innerHTML = ''
+
+
+//create left content
+    let leftcontent = document.createElement('section')
+    leftcontent.classList.add('content-vak')
+    reviewCard.appendChild(leftcontent)
+
+//title inladen
+    let title = document.createElement('h2');
+    title.innerHTML = museadetails[review].name;
+    leftcontent.appendChild(title);
+
+//review title inladen
+    let reviewTitle = document.createElement('h3');
+    reviewTitle.innerHTML = museadetails[review].reviewtitle
+    reviewTitle.id = 'a'
+    leftcontent.appendChild(reviewTitle)
+
+//review rating inladen
+    let reviewRating = document.createElement('h3');
+    reviewRating.innerHTML = museadetails[review].reviewrating
+    reviewRating.id = 'b'
+    leftcontent.appendChild(reviewRating)
+
+//review text inladen
+    let reviewText = document.createElement('h4');
+    reviewText.innerHTML = museadetails[review].reviewtext
+    reviewText.id = 'c'
+    leftcontent.appendChild(reviewText)
+
+//create right content
+    let rightcontent = document.createElement('section')
+    rightcontent.classList.add('content-vak')
+    reviewCard.appendChild(rightcontent)
+
+//title input field inladen
+    let titleInput = document.createElement('h3')
+    titleInput.innerHTML = museadetails[review].titleinput
+    titleInput.id = 'titleinput'
+    const form = document.createElement("FORM");
+    rightcontent.appendChild(form);
+    const x = document.createElement("INPUT");
+    x.setAttribute("type", "text");
+    x.placeholder = "Typ de Title hier";
+    x.id = 'x'
+    form.appendChild(x)
+    form.appendChild(titleInput)
+
+//rating input field inladen
+    let ratingInput = document.createElement('h3')
+    ratingInput.innerHTML = museadetails[review].ratinginput
+    const y = document.createElement("INPUT");
+    y.setAttribute("type", "text");
+    y.placeholder = "Typ de Beoordeling hier";
+    y.id = 'y'
+    form.appendChild(y)
+    form.appendChild(ratingInput)
+
+//review input field inladen
+    let reviewInput = document.createElement('h4')
+    reviewInput.innerHTML = museadetails[review].reviewinput
+    const z = document.createElement("INPUT");
+    z.setAttribute("type", "text");
+    z.placeholder = "Typ de Review hier";
+    z.id = 'z'
+    form.appendChild(z)
+    form.appendChild(reviewInput)
+
+//create post button
+    let postbutton = document.createElement("button")
+    postbutton.innerHTML = 'Post review'
+    postbutton.type = 'submit'
+    form.addEventListener('submit', post);
+    form.dataset.id = museadetails[review].id;
+    form.appendChild(postbutton)
+
+//create close button
+    let closebutton = document.createElement("button")
+    closebutton.innerHTML = 'Lees minder'
+    closebutton.classList.add("lees")
+    rightcontent.appendChild(closebutton)
+}
+
+function post(e){
+    e.preventDefault();
+    inputFieldx = document.getElementById('x')
+    inputFieldy = document.getElementById('y')
+    inputFieldz = document.getElementById('z')
+    let inputValuex = inputFieldx.value;
+    let inputValuey = inputFieldy.value;
+    let inputValuez = inputFieldz.value;
+    if (inputValuex !== '' && inputValuey !== '' && inputValuez !== '') {
+    museadetails[e.target.dataset.id].reviewtitle += inputValuex
+    museadetails[e.target.dataset.id].reviewrating += inputValuey
+    museadetails[e.target.dataset.id].reviewtext += inputValuez
+    let instantpostx = document.getElementById('a')
+        instantpostx.innerHTML = inputValuex
+    let instantposty = document.getElementById('b')
+        instantposty.innerHTML = inputValuey
+    let instantpostz = document.getElementById('c')
+        instantpostz.innerHTML = inputValuez
+        } else {
+        console.log('leeg');
+    }
 }
 
 //create een section
@@ -138,7 +271,11 @@ function detailSuccesHandler(musea) {
     let leesmeerbutton = document.createElement("button")
     leesmeerbutton.innerHTML = 'Lees meer'
     leesmeerbutton.classList.add("lees")
+    leesmeerbutton.dataset.id = musea.id;
     rightcontent.appendChild(leesmeerbutton)
+
+    museadetails[musea.id] = musea
+    console.log(musea)
 }
 
 //error code
@@ -173,8 +310,8 @@ function ItemWasClicked(e) {
         if (detailpage.id === "hide") {
             detailpage.id = "show"
             document.body.classList.add("noscroll")
+            reviewSuccesHandler(e.target.dataset.id)
             console.log(e.target.dataset.id)
-            console.log(museadetails[e.target.dataset.id])
         } else if (detailpage.id === "show") {
             detailpage.id = "hide"
             document.body.classList.remove("noscroll")
